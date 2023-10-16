1e8d97a49c2f04b93a37b7ec1b63216018c65d51
Maher Adil <mahercoder@gmail.com>
2023-10-16 23:49:40 +0500

    Some changes

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
+const PORT = process.env.PORT || 3000
+
 app.get('/connect', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
