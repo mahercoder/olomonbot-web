cabed28c829d51a82b407c87624b5b5a23dcd2a9
Maher Adil <mahercoder@gmail.com>
2023-10-17 02:11:08 +0500

    Some changes

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 const PORT = process.env.PORT || 3000
 
-app.use('/', express.static('public'))
+app.use(express.static('public'))
 
 app.get('/connect', (req, res) => {
   res.sendFile(path.join(__dirname, 'public/index.html'));
